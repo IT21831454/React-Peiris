3ccd3c4b8365cac47e790c7c9211bfc884582eed
Pasindu Peiris <it21831454@my.sliit.lk>
2023-08-30 12:55:23 +0530

    change some details

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Nav from './Components/Nav/Nav';
 
 
 function App() {
   return (
     <div>
+      <Nav/>
       <h1>Pasindu Peiris</h1>
     </div>
   );
---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,14 @@
+import logo512 from './home-banner-1.webp';
+
+function Nav(){
+    return(
+
+        <>
+        
+        <img src={logo512} alt='ldlld' width='1000px'/>        
+        
+        </>
+    );
+}
+
+export default Nav;
